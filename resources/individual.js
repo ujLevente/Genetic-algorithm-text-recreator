e83a1f9ccd637a12016a35af11c1235949f73db6
Uj Levente <asdlevente@gmail.com>
2019-04-22 16:08:05 +0200

    ability to make first generation implemented;

--- resources/individual.js
+++ resources/individual.js
@@ -1,3 +1,26 @@
 class Individual {
+
+    mutationRate = 0.01;
     
+    static get possibleCharacters() {
+        // might switch it to String.toCharcode 
+        return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "; 
+    }
+    
+    constructor(numOfGenomes) {
+        this.genomes = new Array(numOfGenomes);
+    }
+    
+    static getRandomIndividual(sizeOfTargerPhrase) {
+        let individual = new Individual(sizeOfTargerPhrase);
+
+        for (let i = 0; i < sizeOfTargerPhrase; i++) {
+            let randomNum = Math.floor(Math.random() * Individual.possibleCharacters.length);
+            individual.genomes[i] = Individual.possibleCharacters[randomNum];
+        }
+
+        return individual;
+    }
+
+
 }
--- resources/population.js
+++ resources/population.js
@@ -1,24 +1,16 @@
 class Population {
 
-    possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
-
-    constructor(populationSize, targetPhrase) {        
+    constructor(populationSize, targetPhrase) {
         this.populationSize = populationSize;
         this.targetPhrase = targetPhrase.split("");
         this.pop = new Array(populationSize);
-        this.makeFirstPopulation();
+        this.makeFirstGeneration();
     }
 
-    makeFirstPopulation() {
+    makeFirstGeneration() {
         for (let i = 0; i < this.populationSize; i++) {
-
-            let randomPhrase = new Array(this.targetPhrase.length);
-            for (let j = 0; j < this.targetPhrase.length; j++) {
-                let randomNum = Math.floor(Math.random() * this.possibleCharacters.length);
-                randomPhrase[j] = this.possibleCharacters[randomNum];
-            }
-            this.pop[i] = randomPhrase;
-        }        
+            this.pop[i] = Individual.getRandomIndividual(this.targetPhrase.length);
+        }
     }
     
     
