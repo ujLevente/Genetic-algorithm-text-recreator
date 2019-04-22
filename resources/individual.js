f766a79a6f545d9d9296d06125ee86c4a93dee5c
Uj Levente <asdlevente@gmail.com>
2019-04-22 23:28:21 +0200

    cross over function modified; child mutation added;

--- resources/individual.js
+++ resources/individual.js
@@ -2,33 +2,57 @@ class Individual {
 
     mutationRate = 0.01;
     
-    static get possibleCharacters() {
-        // might switch it to String.toCharcode 
-        return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "; 
-    }
-    
     constructor(numOfGenomes) {
         this.genomes = new Array(numOfGenomes);
     }
     
+    static get possibleCharacters() {
+        // might switch it to String.toCharcode 
+        return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "; 
+    }
+        
     static getRandomIndividual(sizeOfTargerPhrase) {
         let individual = new Individual(sizeOfTargerPhrase);
 
         for (let i = 0; i < sizeOfTargerPhrase; i++) {
-            let randomNum = Math.floor(Math.random() * Individual.possibleCharacters.length);
-            individual.genomes[i] = Individual.possibleCharacters[randomNum];
+            let randomIndex = Math.floor(Math.random() * Individual.possibleCharacters.length);
+            individual.genomes[i] = Individual.possibleCharacters[randomIndex];
         }
 
         return individual;
     }
 
-    crossOver(parent1, parent2) {
-        for (i = 0; i < this.genomes.length; i++) {
+    crossOver(partner) {
+        let child = new Individual(this.genomes.length);
+        for (let i = 0; i < this.genomes.length; i++) {
             let randomChoice = Math.round(Math.random());
-            this.genomes[i] = randomChoice === 0 ? parent1.genomes[i] : parent2.genomes[i];
+            child.genomes[i] = randomChoice === 0 ? this.genomes[i] : partner.genomes[i];
         }
-        console.log(this.genomes);
+        this.mutate(child);
         
+        return child;
     }
 
+    mutate(childToMutate) {
+
+        for (let i = 0; i < childToMutate.genomes.length; i++) {
+            let randomChance = Math.random() <= this.mutationRate;
+            
+            if (randomChance) {
+                let randomIndex = Math.floor(Math.random() * Individual.possibleCharacters.length);
+                childToMutate[i] = Individual.possibleCharacters[randomIndex];
+            }
+        }
+    }
+
+    setFittness(targetPhrase) {
+        let score = 0;
+        for (let i = 0; i < this.genomes.length; i++) {
+            if (this.genomes[i] === targetPhrase[i]) {
+                score++;
+            }
+        }
+
+        this.fitness = Math.pow(score, 2);
+    }
 }
